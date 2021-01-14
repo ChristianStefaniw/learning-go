fb865976ca0a5d0ec1ec2da4d79368b7508d1ee1
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-01-13 21:22:28 -0500

    values and types

---
+++ 2 - Values & Types/main.go
@@ -0,0 +1,57 @@
+package main
+
+/*
+Go's basic types
+
+-bool
+
+-string
+
+-int  int8  int16  int32  int64
+-uint uint8 uint16 uint32 uint64 uintptr
+
+-byte // alias for uint8
+
+-rune // alias for int32
+     // represents a Unicode code point
+
+-float32 float64
+
+-complex64 complex128
+ */
+
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe bool = false
+	MaxInt uint64 = 1<<64 - 1
+	z complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+
+	/*
+	Type: bool Value: false
+	Type: uint64 Value: 18446744073709551615
+	Type: complex128 Value: (2+3i)
+	1+1 = 2
+	7.0/3.0 = 2.3333333333333335
+	false
+	true
+	false
+	 */
+}
